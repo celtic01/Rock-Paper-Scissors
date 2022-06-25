11bb15213210c7a85b00f7dc984bc6d0489d4d5a
andreib <andrei.bortas@outlook.com>
2022-06-25 09:21:22 +0300

    Show score of player and computer + refactor code

--- scripts.js
+++ scripts.js
@@ -9,6 +9,12 @@ const playerChoice = document.createElement('div');
 const computerChoice = document.createElement('div');
 const matchDecision = document.createElement('div');
 
+const scoreContainer = document.createElement('div');
+const playerScore = document.createElement('div');
+const computerScore = document.createElement('div');
+
+playerScore.innerText ='you: 0';
+computerScore.innerText ='computer: 0';
 separator.innerText = ':';
 
 let playerSelection = '';
@@ -16,172 +22,43 @@ let computerSelection = '';
 let playerWins = 0;
 let computerWins = 0;
 
+scoreContainer.classList.add('score-container');
 matchDecision.classList.add('match-decision');
 computerChoice.classList.add('computer-choice');
 playerChoice.classList.add('player-choice');
 resultsContainer.classList.add("container-results");
 separator.classList.add("separator");
+playerScore.classList.add('score');
+computerScore.classList.add('score');
+container.classList.add("container");
 
 buttonRock.innerText = 'rock';
 buttonPaper.innerText = 'paper';
 buttonScissors.innerText = 'scissors';
 
 
-container.classList.add("container");
 
+
+scoreContainer.appendChild(playerScore);
+scoreContainer.appendChild(computerScore);
 container.appendChild(buttonRock);
 container.appendChild(buttonPaper);
 container.appendChild(buttonScissors);
 resultsContainer.appendChild(playerChoice);
 resultsContainer.appendChild(separator);
 resultsContainer.appendChild(computerChoice);
+document.body.appendChild(scoreContainer);
 document.body.appendChild(resultsContainer);
 document.body.appendChild(matchDecision);
 document.body.appendChild(container);
 
 
 
-buttonRock.addEventListener('click', () => {
+buttonRock.addEventListener('click', () => Game('rock'));
 
-    matchDecision.removeAttribute('class');
+buttonPaper.addEventListener('click', () => Game('paper'));
 
-    playerChoice.innerText = "rock";
-    playerSelection = "rock";
-    computerChoice.innerText = computerPlay(Math.floor(Math.random() * 3));
-    computerSelection = computerChoice.innerText;
-    matchDecision.innerText = playRound(playerSelection,computerSelection);
-
-    if(matchDecision.innerText == "You won!")
-    {
-        matchDecision.classList.add('match-decision');
-        matchDecision.classList.add('win');
-        playerWins++;
-        console.log("player " + playerWins);
-        if(playerWins == 5)
-        {
-            alert("you won the game, the game will now restart");
-            playerChoice.innerText = "";
-            computerChoice.innerText = "";
-            matchDecision.innerText = "";
-            playerWins = 0;
-            computerWins = 0;
-        }
-
-    }else if (matchDecision.innerText == "You lose!")
-    {
-        matchDecision.classList.add('match-decision');
-        matchDecision.classList.add('loss')
-        computerWins++;
-        console.log("computer " +computerWins);
-        if(computerWins == 5)
-        {
-            alert("you lost the game, the game will now restart");
-            playerChoice.innerText = "";
-            computerChoice.innerText = "";
-            matchDecision.innerText = "";
-            computerWins = 0;
-            playerWins = 0;
-        }
-
-    }
-    else 
-    matchDecision.classList.add('match-decision');
-});
-
-buttonPaper.addEventListener('click', () => {
-
-    matchDecision.removeAttribute('class');
-
-    playerChoice.innerText = "paper";
-    playerSelection = "paper";
-    computerChoice.innerText = computerPlay(Math.floor(Math.random() * 3));
-    computerSelection = computerChoice.innerText;
-    matchDecision.innerText = playRound(playerSelection,computerSelection);
-
-    if(matchDecision.innerText == "You won!")
-    {
-        matchDecision.classList.add('match-decision');
-        matchDecision.classList.add('win');
-        playerWins++;
-        console.log("player " + playerWins);
-        if(playerWins == 5)
-        {
-            alert("you won the game, the game will now restart");
-            playerChoice.innerText = "";
-            computerChoice.innerText = "";
-            matchDecision.innerText = "";
-            playerWins = 0;
-            computerWins = 0;
-        }
-
-    }else if (matchDecision.innerText == "You lose!")
-    {
-        matchDecision.classList.add('match-decision');
-        matchDecision.classList.add('loss')
-        computerWins++;
-        console.log("computer " +computerWins);
-        if(computerWins == 5)
-        {
-            alert("you lost the game, the game will now restart");
-            playerChoice.innerText = "";
-            computerChoice.innerText = "";
-            matchDecision.innerText = "";
-            computerWins = 0;
-            playerWins = 0;
-        }
-
-    }
-    else 
-    matchDecision.classList.add('match-decision');
-});
-
-buttonScissors.addEventListener('click', () => {
-
-    matchDecision.removeAttribute('class');
-
-   
-    playerChoice.innerText = "scissors";
-    playerSelection = "scissors";
-    computerChoice.innerText = computerPlay(Math.floor(Math.random() * 3));
-    computerSelection = computerChoice.innerText;
-    matchDecision.innerText = playRound(playerSelection,computerSelection);
-
-    if(matchDecision.innerText == "You won!")
-    {
-        matchDecision.classList.add('match-decision');
-        matchDecision.classList.add('win');
-        playerWins++;
-        console.log("player " + playerWins);
-        if(playerWins == 5)
-        {
-            alert("you won the game, the game will now restart");
-            playerChoice.innerText = "";
-            computerChoice.innerText = "";
-            matchDecision.innerText = "";
-            playerWins = 0;
-            computerWins = 0;
-        }
-
-    }else if (matchDecision.innerText == "You lose!")
-    {
-        matchDecision.classList.add('match-decision');
-        matchDecision.classList.add('loss')
-        computerWins++;
-        console.log("computer " +computerWins);
-        if(computerWins == 5)
-        {
-            alert("you lost the game, the game will now restart");
-            playerChoice.innerText = "";
-            computerChoice.innerText = "";
-            matchDecision.innerText = "";
-            computerWins = 0;
-            playerWins = 0;
-        }
-
-    }
-    else 
-    matchDecision.classList.add('match-decision');
-});
+buttonScissors.addEventListener('click', () => Game('scissors'));
 
 //console.log(playerChoice());//works
 //console.log(computerPlay(rand));//works
@@ -242,5 +119,55 @@ function playRound(player, computer) {
     }
     else if (player == "scissors" && computer == "rock") {
         return "You lose!";
+    }
+}
+
+function Game(choice) {
+    matchDecision.removeAttribute('class');
+
+    playerChoice.innerText = choice;
+    playerSelection = choice;
+    computerSelection = computerPlay(Math.floor(Math.random() * 3));
+    console.log(`Player chose ${choice}`);
+    computerChoice.innerText = computerSelection;
+
+    matchDecision.innerText = playRound(playerSelection, computerSelection);
+
+    console.log(matchDecision.innerText);
+
+    if (matchDecision.innerText == "You won!") {
+        matchDecision.classList.add('match-decision');
+        matchDecision.classList.add('win');
+        playerWins++;
+        playerScore.innerText = playerScore.innerText.replace(/\d$/, `${playerWins}`);
+        console.log("player " + playerWins);
+        if (playerWins == 5) {
+            alert("you won the game, the game will now restart");
+            playerScore.innerText = 'you: 0';
+            playerChoice.innerText = "";
+            computerChoice.innerText = "";
+            matchDecision.innerText = "";
+            playerWins = 0;
+            computerWins = 0;
+        }
+
+    } else if (matchDecision.innerText == "You lose!") {
+        matchDecision.classList.add('match-decision');
+        matchDecision.classList.add('loss')
+        computerWins++;
+        computerScore.innerText = computerScore.innerText.replace(/\d$/, `${computerWins}`);
+        console.log("computer " + computerWins);
+        if (computerWins == 5) {
+            alert("you lost the game, the game will now restart");
+            computerScore.innerText = "computer: 0";
+            playerChoice.innerText = "";
+            computerChoice.innerText = "";
+            matchDecision.innerText = "";
+            computerWins = 0;
+            playerWins = 0;
+        }
+    }
+    else {
+        matchDecision.classList.add('match-decision');
     }
 }
